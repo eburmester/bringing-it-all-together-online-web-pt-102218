d47854fc917166f2d2759b7fa0a1ea918a1f2df2
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-25 18:40:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,8 +71,9 @@ class Dog
     dog = self.new(id: row[0], name: row[1], breed: row[2])
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    result = DB
+    result = DB[:conn].execute(sql, name)
+  end
  
 end
